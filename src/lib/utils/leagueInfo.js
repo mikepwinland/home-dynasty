c7d44ff26eb5ea931c73ffa5bdc69dba1d3c07a4
mikepwinland <60753227+mikepwinland@users.noreply.github.com>
2024-06-24 08:38:25 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "992195495612964864"; // your league ID
+export const leagueID = "1110664137287270400"; // your league ID
 export const leagueName = "THE HOME DYNASTY"; // your league name
 export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
